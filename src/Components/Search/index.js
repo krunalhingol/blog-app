b18c3d49fa64332eb9138af75f9abacc982f9b7f
Krunal Hingol <krunal.hingol@opcito.com>
2021-04-19 00:19:28 +0530

    - styling adjusted for search box.

--- src/Components/Search/index.js
+++ src/Components/Search/index.js
@@ -5,13 +5,15 @@ const Search = (props) => {
   const { onSearch } = props;
   const onChange = _debounce((e) => onSearch(e.target.value), 1000);
   return (
-    <div className="search-bar">
-      <input
-        className="form-control"
-        type="text"
-        placeholder="Search blog by title"
-        onChange={onChange}
-      />
+    <div className="search-bar row">
+      <div className="col-sm-12">
+        <input
+          className="form-control"
+          type="text"
+          placeholder="Search blog by title"
+          onChange={onChange}
+        />
+      </div>
     </div>
   );
 };
